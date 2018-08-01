ec4e1f31b80ab6bc62b784954c2424fbda10096d
Gabriel Morais <gabrielmorais.sdk@gmail.com>
2018-08-01 16:00:26 -0300

    Salvando novo cliente

--- src/interceptors/error-interceptor.ts
+++ src/interceptors/error-interceptor.ts
@@ -3,6 +3,7 @@ import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HTTP_INTERCEPTORS
 import { Observable } from 'rxjs/Rx'; // IMPORTANTE: IMPORT ATUALIZADO
 import { StorageService } from '../services/storage.service';
 import { AlertController } from 'ionic-angular/components/alert/alert-controller';
+import { FieldMessage } from '../models/fieldmessage';
 
 
 @Injectable()
@@ -33,6 +34,9 @@ export class ErrorInterceptor implements HttpInterceptor {
                     case 401:
                         this.handle401()
                         break
+                    case 422:
+                        this.handle422(errorObj)
+                        break;
                     default:
                         this.handleDefaultError(errorObj)
                 }
@@ -57,6 +61,26 @@ export class ErrorInterceptor implements HttpInterceptor {
             ]
         })
         alert.present()
+    }
+
+    handle422(errorObj) {
+        let alert = this.alertCtrl.create({
+            title: 'Erro de validação',
+            message: this.listErrors(errorObj.errors),
+            enableBackdropDismiss: false,
+            buttons: [{
+                text: 'Ok'
+            }]
+        })
+        alert.present()
+    }
+
+    private listErrors(messages: FieldMessage[]) : string {
+        let s: string = ''
+        for (let i = 0; i < messages.length; i++) {
+            s = s + '<p><strong>' + messages[i].fieldName + '</strong>: ' + messages[i].message + '</p>'            
+        }
+        return s
     }
 
     handleDefaultError(errorObj) {
--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
 import { FormGroup, FormBuilder, Validators } from '../../../node_modules/@angular/forms';
 import { CidadeService } from '../../services/domain/cidade.service';
 import { EstadoService } from '../../services/domain/estado.service';
 import { EstadoDTO } from '../../models/estado.dto';
 import { CidadeDTO } from '../../models/cidade.dto';
+import { ClienteService } from '../../services/domain/cliente.service';
 
 @IonicPage()
 @Component({
@@ -20,7 +21,10 @@ export class SignupPage {
 		public navParams: NavParams,
 		public formBuilder: FormBuilder,
 		public cidadeService: CidadeService,
-		public estadoService: EstadoService) {
+		public estadoService: EstadoService,
+		public clienteService: ClienteService,
+		public alertCtrl: AlertController
+	) {
 
 		this.formGroup = this.formBuilder.group({
 			nome: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(120)]],
@@ -52,7 +56,12 @@ export class SignupPage {
 	}
 
 	signupUser() {
-		console.log('enviou')
+		console.log(this.formGroup.value)
+		this.clienteService.insert(this.formGroup.value)
+			.subscribe(response => {
+				this.showInsertOk()
+			},
+			error => { })
 	}
 
 	updateCidades() {
@@ -63,6 +72,21 @@ export class SignupPage {
 				this.formGroup.controls.cidadeId.setValue(null)
 			},
 			error => {})
+	}
+
+	showInsertOk() {
+		let alert = this.alertCtrl.create({
+			title: 'Sucesso!',
+			message: 'Cadastro realizado com sucesso',
+			enableBackdropDismiss: false,
+			buttons: [{
+				text: 'Ok',
+				handler: () => {
+					this.navCtrl.pop()
+				}
+			}]
+		})
+		alert.present()
 	}
 
 }
