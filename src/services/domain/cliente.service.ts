5bf741f0410aeb79d369afc9921958571efe6abe
Gabriel Morais <gabrielmorais.sdk@gmail.com>
2018-08-02 13:19:27 -0300

    Buscando endereços do banco de dados

--- src/services/domain/cliente.service.ts
+++ src/services/domain/cliente.service.ts
@@ -11,13 +11,13 @@ export class ClienteService {
 
     }
 
-    findByEmail(email: string) : Observable<ClienteDTO> {
-        return this.http.get<ClienteDTO>(`${API_CONFIG.baseUrl}/clientes/email?value=${email}`)
+    findByEmail(email: string) {
+        return this.http.get(`${API_CONFIG.baseUrl}/clientes/email?value=${email}`)
     }
 
-    getImageFromBucket(id: string) : Observable<any> {
+    getImageFromBucket(id: string): Observable<any> {
         let url = `${API_CONFIG.bucketBaseUrl}/cp${id}.jpg`
-        return this.http.get(url, {responseType: 'blob'})
+        return this.http.get(url, { responseType: 'blob' })
     }
 
     insert(obj: ClienteDTO) {
