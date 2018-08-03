fd21b2b854b5e6e847070b936cddeb4c5325000c
Gabriel Morais <gabrielmorais.sdk@gmail.com>
2018-08-03 18:54:54 -0300

    Correção de bug: cancelamento da câmera ou galeria

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -67,6 +67,7 @@ export class ProfilePage {
 			this.picture = 'data:image/png;base64,' + imageData;
 			this.cameraOn = false;
 		}, (err) => {
+			this.cameraOn = false;
 		});
 	}
 
@@ -97,6 +98,7 @@ export class ProfilePage {
 			this.picture = 'data:image/png;base64,' + imageData;
 			this.cameraOn = false;
 		}, (err) => {
+			this.cameraOn = false;
 		});
 	}
 }
