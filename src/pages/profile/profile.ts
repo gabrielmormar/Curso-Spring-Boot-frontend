bbbcef4839f7e1b10712ae8299519bdadf7c3d50
Gabriel Morais <gabrielmorais.sdk@gmail.com>
2018-08-02 22:28:34 -0300

    Upload da foto de usuário

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -8,62 +8,79 @@ import { CameraOptions, Camera } from '../../../node_modules/@ionic-native/camer
 
 @IonicPage()
 @Component({
-  selector: 'page-profile',
-  templateUrl: 'profile.html',
+	selector: 'page-profile',
+	templateUrl: 'profile.html',
 })
 export class ProfilePage {
-  cliente: ClienteDTO
-  picture: string
-  cameraOn: boolean = false
+	cliente: ClienteDTO
+	picture: string
+	cameraOn: boolean = false
 
-  constructor(public navCtrl: NavController,
-    public navParams: NavParams,
-    public storage: StorageService,
-    public clienteService: ClienteService,
-    public camera: Camera) {
-  }
+	constructor(public navCtrl: NavController,
+		public navParams: NavParams,
+		public storage: StorageService,
+		public clienteService: ClienteService,
+		public camera: Camera) {
+	}
 
-  ionViewDidLoad() {
-    let localUser = this.storage.getLocalUser()
-    if (localUser && localUser.email) {
-      this.clienteService.findByEmail(localUser.email)
-        .subscribe(response => {
-          this.cliente = response as ClienteDTO
-          this.getImageIfExists()
-        },
-          error => {
-            if (error.status == 403) {
-              this.navCtrl.setRoot('HomePage')
-            }
-          })
-    }
-    else {
-      this.navCtrl.setRoot('HomePage')
-    }
-  }
+	ionViewDidLoad() {
+		this.loadData()
+	}
 
-  getImageIfExists() {
-    this.clienteService.getImageFromBucket(this.cliente.id)
-      .subscribe(response => {
-        this.cliente.imageUrl = `${API_CONFIG.bucketBaseUrl}/cp${this.cliente.id}.jpg`
-      },
-        error => { })
-  }
+	loadData() {
+		let localUser = this.storage.getLocalUser()
+		if (localUser && localUser.email) {
+			this.clienteService.findByEmail(localUser.email)
+				.subscribe(response => {
+					this.cliente = response as ClienteDTO
+					this.getImageIfExists()
+				},
+					error => {
+						if (error.status == 403) {
+							this.navCtrl.setRoot('HomePage')
+						}
+					})
+		}
+		else {
+			this.navCtrl.setRoot('HomePage')
+		}
+	}
 
-  getCameraPicture() {
-    this.cameraOn = true;
-    const options: CameraOptions = {
-     quality: 100,
-     destinationType: this.camera.DestinationType.DATA_URL,
-     encodingType: this.camera.EncodingType.PNG,
-     mediaType: this.camera.MediaType.PICTURE
-   }
-   
-   this.camera.getPicture(options).then((imageData) => {
-    this.picture = 'data:image/png;base64,' + imageData;
-    this.cameraOn = false;
-   }, (err) => {
-   });
- }
+	getImageIfExists() {
+		this.clienteService.getImageFromBucket(this.cliente.id)
+			.subscribe(response => {
+				this.cliente.imageUrl = `${API_CONFIG.bucketBaseUrl}/cp${this.cliente.id}.jpg`
+			},
+				error => { })
+	}
+
+	getCameraPicture() {
+		this.cameraOn = true;
+		const options: CameraOptions = {
+			quality: 100,
+			destinationType: this.camera.DestinationType.DATA_URL,
+			encodingType: this.camera.EncodingType.PNG,
+			mediaType: this.camera.MediaType.PICTURE
+		}
+
+		this.camera.getPicture(options).then((imageData) => {
+			this.picture = 'data:image/png;base64,' + imageData;
+			this.cameraOn = false;
+		}, (err) => {
+		});
+	}
+
+	sendPicture() {
+		this.clienteService.uploadPicture(this.picture)
+			.subscribe(response => {
+				this.picture = null
+				this.loadData()
+			},
+			error => {})
+	}
+
+	cancel() {
+		this.picture = null
+	}
 
 }
